7de04bf7d953be5a8da9941d963fe51e182f1aa6
Victor Ocnarescu <victorocnarescu@gmail.com>
2019-11-16 18:14:41 +0200

    update headers api

--- src/api/identity/forgot.js
+++ src/api/identity/forgot.js
@@ -2,6 +2,10 @@ import checkStatus from "../check-status"
 
 export default async (data) => {
   return await fetch(`/.netlify/functions/app/forgot`, {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
     body: JSON.stringify(data),
     method: "POST",
   })
--- src/api/identity/login.js
+++ src/api/identity/login.js
@@ -2,6 +2,10 @@ import checkStatus from "../check-status"
 
 export default async (data) => {
   return await fetch(`/.netlify/functions/app/login`, {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
     body: JSON.stringify(data),
     method: "POST",
   })
