4d200f95347b7178e9e7acf58de9fc5cc722c734
Victor Ocnarescu <victorocnarescu@gmail.com>
2019-12-09 19:13:44 +0200

    redirect when token is expired

--- src/components/Identity/PrivateRoute.jsx
+++ src/components/Identity/PrivateRoute.jsx
@@ -2,8 +2,16 @@ import React from "react"
 import PropTypes from "prop-types"
 import { Route, Redirect } from "react-router-dom"
 import { identity } from "../../identity"
+import jwt from "jsonwebtoken"
+import { notify } from "../Toast"
 
 function PrivateRoute({ component: Component, ...rest }) {
+  const decoded = jwt.decode(identity.token)
+  if (decoded && decoded.exp < Date.now() / 1000) {
+    notify.warn("Your session has expired. Please login again")
+    localStorage.removeItem("token")
+  }
+
   return (
     <Route
       {...rest}
