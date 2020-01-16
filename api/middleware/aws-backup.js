3d43e81edb8d8a78653ed85fc19b638bfa4816ef
victorocna <victorocnarescu@gmail.com>
2020-01-16 15:34:47 +0200

    encode logs only for create|update|delete operation

--- api/middleware/aws-backup.js
+++ api/middleware/aws-backup.js
@@ -5,12 +5,13 @@ module.exports = (req, res, next) => {
   const original = res.send
 
   res.send = function(message) {
-    // base64 encode unparsed JSON string
-    if (isJson(message)) {
-      message = Buffer.from(message).toString("base64")
-    }
     if (req.originalUrl.match(/(create|update|delete)/)) {
       const { username, key } = extractJwt(req.headers)
+
+      // base64 encode unparsed JSON string
+      if (isJson(message)) {
+        message = Buffer.from(message).toString("base64")
+      }
 
       // GET /.netlify/functions/app/foo 404 "Not Found" hello@example.com fdss531caz
       putLogEvents(req.method, req.originalUrl, res.statusCode, message, username, key)
