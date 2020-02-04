0a3f4c548f3c15874acb897cc7ff14e98dd09878
victorocna <victorocnarescu@gmail.com>
2020-02-04 13:20:03 +0200

    update tax schema

--- src/schema/tax-schema.js
+++ src/schema/tax-schema.js
@@ -3,7 +3,7 @@ import * as Yup from "yup"
 const taxSchema = Yup.object().shape({
   name: Yup.string().required("Tax name is required"),
   value: Yup.number()
-    .positive("Must be positive")
+    .min(0, "Must not be negative")
     .typeError("Must be a number")
     .required("Tax value is required"),
   tournament: Yup.string().required("Select a tournament for which the tax will be applied"),
