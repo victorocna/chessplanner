5b760c7539c5138df44970238784824583f3b529
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2020-09-08 13:47:04 +0300

    Switch to HashRouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { BrowserRouter as Router } from "react-router-dom"
+import { HashRouter as Router } from "react-router-dom"
 import { makeStyles } from "@material-ui/core/styles"
 import Notifications from "./Toast"
 import Nav from "./Nav"
